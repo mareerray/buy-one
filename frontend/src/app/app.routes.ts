baa1e78cd1475ad289a5b72286ed1a8a7a8ffc49
mareerray <mareerray@gmail.com>
2025-11-14 18:30:55 +0200

    Changed browse sellers to browse collections

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProductListingComponent } from './components/product-listing/product-listing.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 export const routes: Routes = [
   // { path: '', redirectTo: 'signin', pathMatch: 'full' }, // Default to 'signin'
@@ -13,7 +14,8 @@ export const routes: Routes = [
   { path: 'signup', component: SignUpComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'product-listing', component: ProductListingComponent },
-  { path: 'product/:id', component: ProductCardComponent }
+  { path: 'product/:id', component: ProductCardComponent },
+  { path: 'categories', component: CategoriesComponent },
 ];
 
 
