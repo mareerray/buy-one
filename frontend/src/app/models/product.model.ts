211cc7c9badb4ed5ec7e8937db89682cdcad4183
mareerray <mareerray@gmail.com>
2025-11-23 20:58:12 +0200

    Added more mock-products & images

--- frontend/src/app/models/product.model.ts
+++ frontend/src/app/models/product.model.ts
@@ -92,33 +92,32 @@ export const MOCK_PRODUCTS: Product[] = [
     id: '7',
     name: 'Sharingan Ninja Tee',
     description: 'For anime fans and shinobi admirers. Striking eyes and mysterious vibes.',
-    price: 29,
+    price: 35,
     images: [
-      'https://images.unsplash.com/photo-1651087319658-a1923f85551f?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      "assets/images/pixelArt.jpg" 
+      "assets/images/SharinganNinjaTee1.jpg"
     ],
     category: 'anime-pop',
     sellerId: 'USER-004',
     quantity: 10
   },
-  {
+   {
     id: '8',
-    name: 'Dragon Ball Crew Tee',
-    description: 'Bring the energy with iconic Dragon Ball Z characters on your shirt!',
-    price: 32,
+    name: 'Little Warrior Manga Tee',
+    description: 'Power up your style! Featuring a bold manga hero ready for adventure—this tee brings iconic action and nostalgia straight from classic anime pages.',
+    price: 30,
     images: [
-      'https://images.unsplash.com/photo-1736892740703-92cc9088f2a7?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      "assets/images/pixelArt.jpg" 
+      'assets/images/LittleWarriorMangaTee1.png',
+      'assets/images/LittleWarriorMangaTee2.png'
     ],
     category: 'anime-pop',
-    sellerId: 'USER-004',
-    quantity: 7
+    sellerId: 'USER-002',
+    quantity: 6
   },
   {
     id: '9',
     name: 'Meme Cat Pocket Tee',
     description: 'For meme lovers—cheeky pocket cat with a hidden message.',
-    price: 26,
+    price: 25,
     images: [
       'https://images.unsplash.com/photo-1704095371948-58d1f95d8064?q=80&w=2674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       "assets/images/pixelArt.jpg" 
@@ -139,144 +138,90 @@ export const MOCK_PRODUCTS: Product[] = [
   category: 'Gaming & Esports',
   sellerId: 'USER-002',
   quantity: 10
-},
-{
-  id: '11',
-  name: 'Cyber Shooter Tee',
-  description: 'Level up with futuristic style! Perfect for FPS fans.',
-  price: 32,
-  images: [
-    'assets/images/CyberShooterTee1.png',
-    'assets/images/CyberShooterTee2.png'
-  ],
-  category: 'Gaming & Esports',
-  sellerId: 'USER-002',
-  quantity: 8
-},
-{
-  id: '12',
-  name: 'Pro League Tee',
-  description: 'Declare yourself a winner—bold, sharp, and ready for action!',
-  price: 30,
-  images: [
-    'assets/images/ProLeagueTee1.png',
-    'assets/images/ProLeagueTee2.png'
-  ],
-  category: 'Gaming & Esports',
-  sellerId: 'USER-002',
-  quantity: 6
-}
+  },
+  {
+    id: '11',
+    name: 'Cyber Shooter Tee',
+    description: 'Level up with futuristic style! Perfect for FPS fans.',
+    price: 29,
+    images: [
+      'assets/images/CyberShooterTee1.png',
+      'assets/images/CyberShooterTee2.png'
+    ],
+    category: 'Gaming & Esports',
+    sellerId: 'USER-002',
+    quantity: 8
+  },
+  {
+    id: '12',
+    name: 'Pro League Tee',
+    description: 'Declare yourself a winner—bold, sharp, and ready for action!',
+    price: 29,
+    images: [
+      'assets/images/ProLeagueTee1.png',
+      'assets/images/ProLeagueTee2.png'
+    ],
+    category: 'Gaming & Esports',
+    sellerId: 'USER-002',
+    quantity: 6
+  },
+  {
+    id: '13',
+    name: 'Keep Coding Tee',
+    description: 'Show off your coder pride! This shirt features a witty developer print. For anyone who lives on code and caffeine.',
+    price: 28,
+    images: [
+      'assets/images/KeepCodingTee1.png',
+      'assets/images/KeepCodingTee2.png'
+    ],
+    category: 'code-nerd',
+    sellerId: 'USER-004',
+    quantity: 6
+  },
+  {
+    id: '14',
+    name: 'Groupie-Tracker Tee',
+    description: 'Geek out in style! This tee sports a fun, graph-inspired design with curly braces and clever programmer flair.',
+    price: 22,
+    images: [
+      'assets/images/GroupieTrackerTee1.png',
+      'assets/images/GroupieTrackerTee2.png'
+
+    ],
+    category: 'code-nerd',
+    sellerId: 'USER-004',
+    quantity: 6
+  },
+  {
+    id: '15',
+    name: 'Debug Life Meme Tee',
+    description: 'Embrace the daily struggle of every coder! "Why isn’t it working?" gesture. Relatable, lighthearted, and perfect for debugging sessions.',
+    price: 25,
+    images: [
+      'assets/images/DebugLifeMemeTee1.png',
+      'assets/images/DebugLifeMemeTee2.png'
+    ],
+    category: 'geeky-memes',
+    sellerId: 'USER-004',
+    quantity: 6
+  },
+  {
+    id: '16',
+    name: 'Totoro Night Pop Tee',
+    description: 'Step into a magical night with Totoro! This tee features the beloved forest spirit in glowing, dreamy colors—perfect for anime fans, dreamers, and Studio Ghibli lovers.',
+    price: 30,
+    images: [
+      'assets/images/TotoroNightPopTee1.png',
+      'assets/images/TotoroNightPopTee2.jpg'
+    ],
+    category: 'anime-pop',
+    sellerId: 'USER-002',
+    quantity: 6
+  }
 ];
 
 /*
 {
-  id: '14',
-  name: 'Action Noir Tee',
-  description: 'Make a statement—bold, fearless, and ready for the night. Action-movie vibes in every thread!',
-  price: 30,
-  images: [
-    'assets/images/10.jpg'
-  ],
-  category: 'limited-editions',
-  sellerId: 'USER-002',
-  quantity: 6
-},
-{
-  id: '15',
-  name: 'Martial Arts Promo Tee',
-  description: 'Show off power and skill—a tee inspired by martial arts legends, perfect for those with fighting spirit.',
-  price: 30,
-  images: [
-    'assets/images/9.jpg'
-  ],
-  category: 'limited-editions',
-  sellerId: 'USER-002',
-  quantity: 6
-}
-{
-  id: '7',
-  name: 'Sharingan Ninja Tee',
-  description: 'For anime fans and shinobi admirers. Striking eyes and mysterious vibes.',
-  price: 29,
-  images: [
-    'andrea-de-santis-1ZlOGMp6Eu0-unsplash.jpg'
-  ],
-  category: 'anime-pop',
-  sellerId: 'USER-001',
-  quantity: 10
-},
-{
-  id: '8',
-  name: 'Dragon Ball Crew Tee',
-  description: 'Bring the energy with iconic Dragon Ball Z characters on your shirt!',
-  price: 32,
-  images: [
-    'akshay-chauhan-GQ62Gz6-BFg-unsplash.jpg'
-  ],
-  category: 'anime-pop',
-  sellerId: 'USER-001',
-  quantity: 7
-},
-{
-  id: '9',
-  name: 'Meme Cat Pocket Tee',
-  description: 'For meme lovers—cheeky pocket cat with a hidden message.',
-  price: 26,
-  images: [
-    'max-herr-EHyJ7F-8oEQ-unsplash.jpg'
-  ],
-  category: 'geeky-memes',
-  sellerId: 'USER-001',
-  quantity: 15
-}
 
-{
-  id: '10',
-  name: 'Esports Champion Tee',
-  description: 'Unleash your gaming spirit with this esports-inspired design!',
-  price: 29,
-  images: [
-    'assets/images/1.jpg'
-  ],
-  category: 'Gaming & Esports',
-  sellerId: 'USER-002',
-  quantity: 10
-},
-{
-  id: '11',
-  name: 'Cyber Shooter Tee',
-  description: 'Level up with futuristic style! Perfect for FPS fans.',
-  price: 32,
-  images: [
-    'assets/images/2.jpg'
-  ],
-  category: 'Gaming & Esports',
-  sellerId: 'USER-002',
-  quantity: 8
-},
-{
-  id: '12',
-  name: 'Pro League Tee',
-  description: 'Declare yourself a winner—bold, sharp, and ready for action!',
-  price: 30,
-  images: [
-    'assets/images/3.jpg'
-  ],
-  category: 'Gaming & Esports',
-  sellerId: 'USER-002',
-  quantity: 6
-},
-{
-  id: '4',
-  name: 'Bot Coder Tee',
-  description: 'Geek out in style with this coding robot design!',
-  price: 28,
-  images: [
-    'assets/images/5.jpg'
-  ],
-  category: 'Code & Nerd Humor',
-  sellerId: 'USER-002',
-  quantity: 12
-},
 
 */
